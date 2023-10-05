09bd7e6eff42486712270908e1d48e50e792a390
Evan Berg <evan.m.berg@gmail.com>
2023-10-05 13:08:32 -0400

    believes i got espn data good

--- players_compare_tool.py
+++ players_compare_tool.py
@@ -15,8 +15,8 @@ APIS = {
 def tank_parser(url, headers, p1, p2):
   players = [p1, p2]
   players_data = []
-  for item in players:
-    data = requests.get(url, headers=headers, params=item).json()['body'][0]
+  for player in players:
+    data = requests.get(url, headers=headers, params=player).json()['body'][0]
     tank_data = {'tank': {
       'name': data['espnName'],
       'position': data['pos'],
@@ -31,27 +31,53 @@ def tank_parser(url, headers, p1, p2):
 def espn_parser(url, p1_id, p2_id, p1_pos, p2_pos):
   split_url = url.split('pid')
   player_list = [{"p_id": p1_id, "pos": p1_pos}, {"p_id": p2_id, "pos": p2_pos}]
+  espn_data = {'espn': {}}
   players_data = []
-  for index, item in enumerate(player_list):
-    i = index + 1
-    pos = item['pos']
-    espn_url = split_url[0] + item['p_id'] + split_url[1]
+  for player in player_list:
+    espn_url = split_url[0] + player['p_id'] + split_url[1]
     data = requests.get(espn_url).json()['splits']['categories']
-    general = next(item for item in data if item['displayName'] == 'General')['stats']
-    fumbles = next(item for item in general if item['displayName'] == 'Fumbles')['value']
-    games_played = next(item for item in general if item['displayName'] == 'Games Played')['value']
-    scoring = next(item for item in data if item['displayName'] == 'Scoring')['stats']
-    receiving = next(item for item in data if item['displayName'] == 'Receiving')['stats']
-    rushing = next(item for item in data if item['displayName'] == 'Rushing')['stats']
-    passing = next(item for item in data if item['displayName'] == 'Passing')['stats']
-    espn_data = {'espn': {
-      'scoring': scoring,
-      'games_played': games_played,
-      'fumbles': fumbles,
-      'receiving': receiving,
-      'rushing': rushing,
-      'passing': passing
-    }}
+    for item in data:
+      cat = item['displayName']
+      if cat == 'General':
+        espn_data['espn'][cat] = {}
+        stats = item['stats']
+        espn_data['espn'][cat]['games_played'] = next(i for i in stats if i['name'] == 'gamesPlayed')['value']
+        espn_data['espn'][cat]['fumbles'] = next(i for i in stats if i['name'] == 'fumbles')['value']
+      elif (p1_pos == 'QB' and p2_pos == 'QB') and cat == 'Passing':
+        espn_data['espn'][cat] = {}
+        stats = item['stats']
+        stat_names = ['completionPct', 'completions', 'interceptionPct', 'interceptions', 'passingAttempts', 'passingBigPlays', 'passingTouchdownPct', 'passingTouchdowns', 'passingYards', 
+                  'passingYardsAtCatch', 'passingYardsPerGame', 'QBRating', 'sacks', 'sackYardsLost', 'totalTouchdowns', 'totalYardsFromScrimmage', 'yardsFromScrimmagePerGame', 'yardsPerGame']
+        for i in stats:
+          name = i['name']
+          if name in stat_names:
+            espn_data['espn'][cat][name] = i['value']
+      elif (p1_pos != 'QB' and p2_pos != 'QB') and cat == 'Rushing':
+        espn_data['espn'][cat] = {}
+        stats = item['stats']
+        stat_names = ['ESPNRBRating', 'netYardsPerGame', 'rushingAttempts', 'rushingBigPlays', 'rushingFirstDowns', 'rushingFirstDowns', 'rushingYardsPerGame', 'rushingTouchdowns', 
+                      'stuffs', 'totalTouchdowns', 'totalYards', 'yardsPerGame', 'yardsPerRushAttempt']
+        for i in stats:
+          name = i['name']
+          if name in stat_names:
+            espn_data['espn'][cat][name] = i['value']
+      elif (p1_pos != 'QB' and p2_pos != 'QB') and cat == 'Receiving':
+        espn_data['espn'][cat] = {}
+        stats = item['stats']
+        stat_names = ['ESPNWRRating', 'netTotalYards', 'netYardsPerGame', 'receivingBigPlays', 'receivingTargets', 'receivingTouchdowns', 'receivingYards', 'receivingYardsAfterCatch',
+                      'receivingYardsPerGame', 'receptions', 'totalPointsPerGame', 'totalTouchdowns', 'totalYards', 'yardsPerGame', 'yardsPerReception']
+        for i in stats:
+          name = i['name']
+          if name in stat_names:
+            espn_data['espn'][cat][name] = i['value']
+      # maybe keep scoring as special teams? not sure how important this data is yet
+      # elif cat == 'Scoring':
+      #   espn_data['espn']['special_teams'] = {}
+      #   stats = item['stats']
+      #   stat_names = []
+      #   ipdb.set_trace()
+
+
     players_data.append(espn_data)
   return players_data
 
@@ -82,10 +108,12 @@ def compare_players(player1, player2):
       espn_data = espn_parser(url, p1_espn_id, p2_espn_id, p1_pos, p2_pos)
       player1_list.append(espn_data[0])
       player2_list.append(espn_data[1])
-  ipdb.set_trace()
-  
-    # pprint.pprint(player1_list)
-    # pprint.pprint(player2_list)
+  # ipdb.set_trace()
+  print(player1_fullname)  
+  pprint.pprint(player1_list)
+  print('---------------------------------------------------------------------')
+  print(player2_fullname)
+  pprint.pprint(player2_list)
 
 def main():
   player1 = sys.argv[1]
